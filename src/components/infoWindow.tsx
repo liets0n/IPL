184a3dde6e7e0ed4b0f29bd27d11a2f8d880eb8c
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2026-06-03 02:01:31 +0100

    refactor: Adjustments in the information window

--- src/components/infoWindow.tsx
+++ src/components/infoWindow.tsx
@@ -1,10 +1,21 @@
+/** biome-ignore-all lint/suspicious/noShadowRestrictedNames: s */
+
 'use client'
 
 import { useTranslations } from 'next-intl'
-
 import { Window } from '@/components'
 import { useInfo } from '@/store'
 import { textFormatter } from '@/utils'
+
+const Item = ({ title, content }: { title: string; content: string }) => {
+  return (
+    <li className='leading-[2.1rem] [&>span]:text-base'>
+      <span className='font-bold text-gray-800'>{title}</span>
+      &nbsp;
+      <span>{content}</span>
+    </li>
+  )
+}
 
 export default function InfoWindow() {
   const t = useTranslations('home')
@@ -13,143 +24,55 @@ export default function InfoWindow() {
 
   return (
     <Window windowTitle={t('window.title.information')}>
-      <div className='[grid-area:info-window] pt-2 pr-4 pb-4 pl-8'>
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.ipv4')}</span>
-            &nbsp;
-            <span>{textFormatter(data.ip, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.city')}</span>
-            &nbsp;
-            <span>{textFormatter(data.city, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.state')}</span>
-            &nbsp;
-            <span>{textFormatter(data.region, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.country')}</span>
-            &nbsp;
-            <span>
-              {textFormatter(`${data.country_name} ${data.emoji_flag}`, t)}
-            </span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>
-              {t('info.continent')}
-            </span>
-            &nbsp;
-            <span>{textFormatter(data.continent_name, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.time')}</span>
-            &nbsp;
-            <span>{textFormatter(time.toLocaleString(), t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>
-              {t('info.latitude')}
-            </span>
-            &nbsp;
-            <span>{textFormatter(data.latitude, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>
-              {t('info.longitude')}
-            </span>
-            &nbsp;
-            <span>{textFormatter(data.longitude, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.idiom')}</span>
-            &nbsp;
-            <span>
-              {textFormatter(
-                Array.isArray(data.languages) && data.languages.length > 0
-                  ? `${data.languages[0].name} (${String(data.languages[0].code).toUpperCase()})`
-                  : 'N/A',
-                t
-              )}
-            </span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>
-              {t('info.currency')}
-            </span>
-            &nbsp;
-            <span>
-              {textFormatter(
-                `${data.currency.name} (${data.currency.code})`,
-                t
-              )}
-            </span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.ddd')}</span>
-            &nbsp;
-            <span>{textFormatter(`${data.calling_code}`, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.tor')}</span>
-            &nbsp;
-            <span>{textFormatter(data.threat.is_tor, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>{t('info.proxy')}</span>
-            &nbsp;
-            <span>{textFormatter(data.threat.is_proxy, t)}</span>
-          </p>
-        </li>
-
-        <li>
-          <p className='leading-[2.1rem] [&>span]:text-base'>
-            <span className='font-bold text-gray-800'>
-              {t('info.dataCenter')}
-            </span>
-            &nbsp;
-            <span>{textFormatter(data.threat.is_datacenter, t)}</span>
-          </p>
-        </li>
-      </div>
+      <ul className='[grid-area:info-window] pt-2 pr-4 pb-4 pl-4'>
+        <Item title={t('info.city')} content={textFormatter(data.city, t)} />
+        <Item title={t('info.state')} content={textFormatter(data.region, t)} />
+        <Item
+          title={t('info.country')}
+          content={textFormatter(`${data.country_name} ${data.emoji_flag}`, t)}
+        />
+        <Item
+          title={t('info.continent')}
+          content={textFormatter(data.continent_name, t)}
+        />
+        <Item
+          title={t('info.time')}
+          content={textFormatter(time.toLocaleString(), t)}
+        />
+        <Item
+          title={t('info.latitude')}
+          content={textFormatter(data.latitude, t)}
+        />
+        <Item
+          title={t('info.longitude')}
+          content={textFormatter(data.longitude, t)}
+        />
+        <Item
+          title={t('info.idiom')}
+          content={textFormatter(
+            Array.isArray(data.languages) && data.languages.length > 0
+              ? `${data.languages[0].name} (${String(data.languages[0].code).toUpperCase()})`
+              : 'N/A',
+            t
+          )}
+        />
+        <Item
+          title={t('info.ddd')}
+          content={textFormatter(`${data.calling_code}`, t)}
+        />
+        <Item
+          title={t('info.tor')}
+          content={textFormatter(data.threat.is_tor, t)}
+        />
+        <Item
+          title={t('info.proxy')}
+          content={textFormatter(data.threat.is_proxy, t)}
+        />
+        <Item
+          title={t('info.dataCenter')}
+          content={textFormatter(data.threat.is_datacenter, t)}
+        />
+      </ul>
     </Window>
   )
 }
